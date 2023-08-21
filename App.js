5f0b076d890fb505a196d59d884d72b6b7d3f963
shinighami <idimaayomide@gmail.com>
2023-08-21 11:37:39 +0100

    added the app nam=vigator

--- App.js
+++ App.js
@@ -1,31 +1,22 @@
 import { PaperProvider } from "react-native-paper";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { DashboardScreen } from "./screen/Dashboard";
-import { CartScreen } from "./screen/Carts";
-import { DetailScreen } from "./screen/Details";
-import { CartHeader, DashboardHeader, DetailHeader } from "./components/header";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import { useFonts } from 'expo-font'
-
-const Stack = createNativeStackNavigator();
+import AppNavigator from "./navigator/AppNavigator";
 export default function App() {
 
   const [loaded] = useFonts({
     Ubuntu: require('./assets/fonts/Ubuntu-Regular.ttf')
   });
-  
 
   if (!loaded) {
     return null;
   }
 
   return (
-    
     <PaperProvider>
       <Provider store={store}>
-      
+        <AppNavigator/>
       </Provider>
     </PaperProvider>
   );
